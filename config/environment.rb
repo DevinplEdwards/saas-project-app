93baccf9b79e90637093052affbc7b6a4fe7f60f
devinple <devinpledwards@live.com>
2022-08-25 02:46:57 +0000

    db config

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
+    :port => '3000',
+    :authentication => :plain,
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'localhost',
+    :enable_starttls_auto => true
+}
